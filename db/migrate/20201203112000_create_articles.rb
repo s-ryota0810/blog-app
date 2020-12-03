f20b504c5c80a72431d7976b97643379b90f1353
ryota.s.0810 <akaashi0810@gmail.com>
2020-12-03 20:27:31 +0900

    Articleモデルを作成した

---
+++ db/migrate/20201203112000_create_articles.rb
@@ -0,0 +1,9 @@
+class CreateArticles < ActiveRecord::Migration[6.0]
+  def change
+    create_table :articles do |t|
+      t.string :title #短い文字列はstring
+      t.text :content #長い文字列はtext
+      t.timestamps  #デフォルト
+    end
+  end
+end
