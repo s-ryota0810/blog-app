4e1b60aa4256ec1a924571aea3a2d3b6ecb65fc9
ryota.s.0810 <akaashi0810@gmail.com>
2020-12-08 23:08:04 +0900

    has_many, belongs_to を記述

--- app/models/article.rb
+++ app/models/article.rb
@@ -21,6 +21,8 @@ class Article < ApplicationRecord
   validates :content, presence: true, length: { minimum: 10 }, uniqueness: true
 
   validate :validate_title_and_content_length
+  
+  belongs_to :user
 
   def display_created_at
     I18n.l(created_at, formats: :default)
