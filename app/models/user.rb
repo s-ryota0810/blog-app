007232369f8a15dadc9ee14e438454a4816646ae
ryota.s.0810 <akaashi0810@gmail.com>
2020-12-12 14:59:57 +0900

    画像アップロードと表示

--- app/models/user.rb
+++ app/models/user.rb
@@ -62,4 +62,12 @@ class User < ApplicationRecord
     profile || build_profile
   end
   
+  def avatar_image
+    if profile&.avatar&.attached?
+      profile.avatar
+    else
+      'default-avatar.png'
+    end
+  end
+  
 end
