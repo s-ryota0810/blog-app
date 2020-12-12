009616c3f694c2eace4e6ab032750c912e938e27
ryota.s.0810 <akaashi0810@gmail.com>
2020-12-12 15:19:24 +0900

    プロフィールがない時の修正②

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -23,7 +23,7 @@ class Profile < ApplicationRecord
   
   
   def age
-    return '不明' unless 
+    return '不明' unless birthday.present?
     years = Time.zone.now.year - birthday.year
     days = Time.zone.now.yday - birthday.yday
     if days < 0
