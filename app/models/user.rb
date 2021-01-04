d12249170590847d7b9c8f26d0c66f289cea5cac
ryota.s.0810 <akaashi0810@gmail.com>
2021-01-05 00:39:56 +0900

    フォロワーの関係

--- app/models/user.rb
+++ app/models/user.rb
@@ -28,7 +28,8 @@ class User < ApplicationRecord
   has_one :profile, dependent: :destroy
   has_many :following_relationships, foreign_key: 'follower_id', class_name: 'Relationship', dependent: :destroy
   has_many :followings, through: :following_relationships, source: :following
-  
+  has_many :follower_relationships, foreign_key: 'following_id', class_name: 'Relationship', dependent: :destroy
+  has_many :followers, through: :follower_relationships, source: :follower
   delegate :birthday, :gender, :introduction, :age, to: :profile, allow_nil: true
 
   def has_written?(article)
