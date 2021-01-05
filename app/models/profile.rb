5d9dbd685933e014b1867f11ee631a8b450c0110
ryota.s.0810 <akaashi0810@gmail.com>
2021-01-05 23:38:16 +0900

    プロフィールページを修正

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -27,7 +27,7 @@ class Profile < ApplicationRecord
     years = Time.zone.now.year - birthday.year
     days = Time.zone.now.yday - birthday.yday
     if days < 0
-      "#{years - 1}歳}"
+      "#{years - 1}歳"
     else
       "#{years}歳"
     end
