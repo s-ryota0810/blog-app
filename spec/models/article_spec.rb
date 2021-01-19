999989b3bb975887cced58d5833fc312b40e188f
ryota.s.0810 <akaashi0810@gmail.com>
2021-01-19 22:19:07 +0900

    保存に失敗する場合のテストを追加

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,20 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Article, type: :model do
+  let!(:user) do
+    User.create!({
+      email: 'test@example.com',
+      password: 'password'
+    })
+  end
+  
   context 'If title and content are entered' do
-    let!(:user) do
-      User.create!({
-        email: 'test@example.com',
-        password: 'password'
-      })
-    end
-    
     let!(:article) do
       user.articles.build({
         title: Faker::Lorem.characters(number: 10),
         content: Faker::Lorem.characters(number: 300)
       })
     end
+  
 #    before do
 #      user = User.create!({
 #        email: 'test@eample.com',
@@ -25,9 +26,20 @@ RSpec.describe Article, type: :model do
 #        content: Faker::Lorem.characters(number: 300)
 #      })      
 #    end
-    
     it 'The article can be saved' do
       expect(article).to be_valid
+    end
+  end
+  
+  context 'If the title has a single character' do
+    let!(:article) do
+      user.articles.create({
+        title: Faker::Lorem.characters(number: 1),
+        content: Faker::Lorem.characters(number: 300)
+      })
+    end
+    it 'The article cannot be saved' do
+      expect(article.errors.messages[:title][0]).to eq('は2文字以上で入力してください')
     end
   end
 end
