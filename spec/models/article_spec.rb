1ab3e80a72f5a148012dc5353471eda2ac3dcaa5
ryota.s.0810 <akaashi0810@gmail.com>
2021-01-19 21:52:41 +0900

    モデルのテストを書いてみよう

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,4 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Article, type: :model do
+  it 'The article can be saved if title and content are entered' do
+    user = User.create!({
+      email: 'test@eample.com',
+      password: 'password'
+    })
+    article = user.articles.build({
+      title: Faker::Lorem.characters(number: 10),
+      content: Faker::Lorem.characters(number: 300)
+    })
+    
+    expect(article).to be_valid
+  end
 end
