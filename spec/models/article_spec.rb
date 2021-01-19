4f03c4ea8717a2324c0c5c47dfadd049fe4cb3c4
ryota.s.0810 <akaashi0810@gmail.com>
2021-01-19 21:58:18 +0900

    context

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,16 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Article, type: :model do
-  it 'The article can be saved if title and content are entered' do
-    user = User.create!({
-      email: 'test@eample.com',
-      password: 'password'
-    })
-    article = user.articles.build({
-      title: Faker::Lorem.characters(number: 10),
-      content: Faker::Lorem.characters(number: 300)
-    })
+  context 'If title and content are entered' do
+    before do
+      user = User.create!({
+        email: 'test@eample.com',
+        password: 'password'
+      })
+      @article = user.articles.build({
+        title: Faker::Lorem.characters(number: 10),
+        content: Faker::Lorem.characters(number: 300)
+      })      
+    end
     
-    expect(article).to be_valid
+    it 'The article can be saved' do
+      expect(@article).to be_valid
+    end
   end
 end
