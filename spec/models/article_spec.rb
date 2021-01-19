d10c128854265af23ad5122031109071b401a710
ryota.s.0810 <akaashi0810@gmail.com>
2021-01-19 22:03:13 +0900

    let

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -2,19 +2,32 @@ require 'rails_helper'
 
 RSpec.describe Article, type: :model do
   context 'If title and content are entered' do
-    before do
-      user = User.create!({
-        email: 'test@eample.com',
+    let!(:user) do
+      User.create!({
+        email: 'test@example.com',
         password: 'password'
       })
-      @article = user.articles.build({
-        title: Faker::Lorem.characters(number: 10),
-        content: Faker::Lorem.characters(number: 300)
-      })      
     end
     
+    let!(:article) do
+      user.articles.build({
+        title: Faker::Lorem.characters(number: 10),
+        content: Faker::Lorem.characters(number: 300)
+      })
+    end
+#    before do
+#      user = User.create!({
+#        email: 'test@eample.com',
+#        password: 'password'
+#      })
+#      @article = user.articles.build({
+#        title: Faker::Lorem.characters(number: 10),
+#        content: Faker::Lorem.characters(number: 300)
+#      })      
+#    end
+    
     it 'The article can be saved' do
-      expect(@article).to be_valid
+      expect(article).to be_valid
     end
   end
 end
